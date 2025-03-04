0656bff4ab0cb0e9f0bfe4da4c03fe0fc5b8fbe0
poleshkkkaa <polinapolesko63@gmail.com>
2025-03-04 18:28:23 +0200

    work with LINQ

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] A = { "apple", "bank", "cherry", "dog" };
+
+        var result = A.Reverse().Select(w => w[0]);
+
+        foreach (var latters in result)
+        {
+            Console.Write(latters + " ");
+        }
+    }
+}
